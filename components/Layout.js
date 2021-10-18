6ce200771d439c6e37d60ab98b82e026a3cb1663
shiv <shivkanthb@gmail.com>
2021-10-17 17:35:59 -0700

    fonts and styling

---
+++ components/Layout.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/GT-Maru-Bold-Trial.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/GT-Maru-Black-Trial.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <meta name="viewport" content="width=device-width" />
+        <meta charSet="utf-8" />
+        <title>web3 starter</title>
+        <meta name="description" content="web3 starter nextjs - shiv" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
+      {children}
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { Web3Provider } from "@ethersproject/providers";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { MetamaskIcon, WalletConnectIcon } from "../components/icons";
+import Layout from "../components/Layout";
 
 const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] });
 const wcConnector = new WalletConnectConnector({
@@ -97,149 +98,148 @@ function Home() {
   };
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>web3 starter</title>
-        <meta name="description" content="web3 starter nextjs" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {!web3React.active ? (
-        <div className="connect-wallet-container">
-          <div className="connect-wallet-card">
-            <div className="wallet-header">Connect your wallet</div>
-            <div
-              className="button metamask"
-              onClick={() => {
-                setLatestConnector(ConnectorNames.Injected);
-                setLatestOp(W3Operations.Connect);
-                web3React.activate(injected);
-              }}
-            >
-              Metamask
-              <MetamaskIcon />
-            </div>
-            <div
-              className="button walletconnect"
-              onClick={() => {
-                setLatestConnector(ConnectorNames.WalletConnect);
-                setLatestOp(W3Operations.Connect);
-                web3React.activate(wcConnector);
-              }}
-            >
-              WalletConnect
-              <WalletConnectIcon />
-            </div>
-          </div>
-        </div>
-      ) : null}
-
-      {web3React.active ? (
-        <>
-          <div className="connected-container">
-            <div className="connected-card">
-              <div className="row network-section">
-                <div className="row-title">Network</div>
-                <div className="row-subtitle">
-                  {getNetwork(web3React.chainId)}
-                </div>
-              </div>
-              <hr className="divider" />
-              <div className="row network-section">
-                <div className="row-title">Address</div>
-                <div className="row-subtitle">
-                  {getTruncatedAddress(web3React.account)}
-                </div>
-              </div>
-              <hr className="divider" />
+    <layout>
+      <div className={styles.container}>
+        {!web3React.active ? (
+          <div className="connect-wallet-container">
+            <div className="connect-wallet-card">
+              <div className="wallet-header">Connect your wallet</div>
               <div
-                className="row disconnect-button"
+                className="button metamask"
                 onClick={() => {
-                  setLatestOp(W3Operations.Disconnect);
-                  web3React.deactivate();
+                  setLatestConnector(ConnectorNames.Injected);
+                  setLatestOp(W3Operations.Connect);
+                  web3React.activate(injected);
                 }}
               >
-                Disconnect
+                Metamask
+                <MetamaskIcon />
+              </div>
+              <div
+                className="button walletconnect"
+                onClick={() => {
+                  setLatestConnector(ConnectorNames.WalletConnect);
+                  setLatestOp(W3Operations.Connect);
+                  web3React.activate(wcConnector);
+                }}
+              >
+                WalletConnect
+                <WalletConnectIcon />
               </div>
             </div>
           </div>
-        </>
-      ) : null}
+        ) : null}
 
-      <style jsx>{`
-        .connect-wallet-container {
-          display: flex;
-          width: 400px;
-          height: 300px;
-          border-radius: 30px;
-          background: #ffffff;
-          justify-content: center;
-          align-items: center;
-          text-align: center;
-          box-shadow: rgb(0 0 0 / 10%) 0px 4px 20px;
-        }
-        .wallet-header {
-          margin-bottom: 30px;
-        }
-        .button {
-          width: 300px;
-          height: 60px;
-          background: #ffffff;
-          box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
-          border-radius: 10px;
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          padding: 0 25px;
-          margin: 20px auto;
-        }
-        .button:hover {
-          cursor: pointer;
-        }
+        {web3React.active ? (
+          <>
+            <div className="connected-container">
+              <div className="connected-card">
+                <div className="row network-section">
+                  <div className="row-title">Network</div>
+                  <div className="row-subtitle">
+                    {getNetwork(web3React.chainId)}
+                  </div>
+                </div>
+                <hr className="divider" />
+                <div className="row network-section">
+                  <div className="row-title">Address</div>
+                  <div className="row-subtitle">
+                    {getTruncatedAddress(web3React.account)}
+                  </div>
+                </div>
+                <hr className="divider" />
+                <div
+                  className="row disconnect-button"
+                  onClick={() => {
+                    setLatestOp(W3Operations.Disconnect);
+                    web3React.deactivate();
+                  }}
+                >
+                  Disconnect
+                </div>
+              </div>
+            </div>
+          </>
+        ) : null}
 
-        .connected-container {
-          display: flex;
-          margin: 20px auto;
-          width: 400px;
-          border-radius: 30px;
-          background: #ffffff;
-          box-shadow: rgb(0 0 0 / 10%) 0px 4px 20px;
-        }
+        <style jsx>{`
+          .connect-wallet-container {
+            display: flex;
+            width: 400px;
+            height: 300px;
+            border-radius: 30px;
+            background: #ffffff;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            box-shadow: rgb(0 0 0 / 10%) 0px 4px 20px;
+          }
+          .wallet-header {
+            font-size: 20px;
+            font-weight: 700;
+            margin-bottom: 30px;
+          }
+          .button {
+            width: 300px;
+            height: 60px;
+            background: #ffffff;
+            box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+            border-radius: 10px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 0 25px;
+            margin: 20px auto;
+          }
+          .button:hover {
+            cursor: pointer;
+          }
 
-        .row {
-          display: flex;
-          flex-direction: column;
-          height: 80px;
-          width: 400px;
-          justify-content: center;
-          padding: 0 20px;
-        }
+          .connected-container {
+            display: flex;
+            /* margin: 20px auto; */
+            width: 400px;
+            border-radius: 30px;
+            background: #ffffff;
+            box-shadow: rgb(0 0 0 / 10%) 0px 4px 20px;
+          }
 
-        .row-title {
-          font-size: 16px;
-          color: #afafaf;
-          font-weight: 900;
-        }
-        .row-subtitle {
-          font-size: 22px;
-          font-weight: 700;
-        }
-        .disconnect-button {
-          align-items: center;
-          color: #f96666;
-          font-size: 20px;
-          font-weight: 900;
-        }
-        .disconnect-button:hover {
-          cursor: pointer;
-        }
+          .row {
+            display: flex;
+            flex-direction: column;
+            height: 80px;
+            width: 400px;
+            justify-content: center;
+            padding: 0 20px;
+          }
 
-        .divider {
-          height: 0.1px;
-          background-color: #e5e5e5;
-          border: none;
-          margin: unset;
-        }
-      `}</style>
-    </div>
+          .row-title {
+            font-size: 16px;
+            color: #afafaf;
+            font-weight: 900;
+          }
+          .row-subtitle {
+            font-size: 22px;
+            font-weight: 700;
+          }
+          .disconnect-button {
+            align-items: center;
+            color: #f96666;
+            font-size: 20px;
+            font-weight: 900;
+          }
+          .disconnect-button:hover {
+            cursor: pointer;
+          }
+
+          .divider {
+            height: 0.1px;
+            background-color: #e5e5e5;
+            border: none;
+            margin: unset;
+          }
+        `}</style>
+      </div>
+    </layout>
   );
 }
